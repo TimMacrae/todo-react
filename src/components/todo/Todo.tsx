55679adea25c2d6fc484e59cf95b96c002db2f1e
Tim_Macrae <55285070+TimMacrae@users.noreply.github.com>
2025-08-13 15:54:05 +0200

    feat: create Todo component to display individual tasks with status

---
+++ src/components/todo/Todo.tsx
@@ -0,0 +1,24 @@
+import type {Todo} from "./types.ts";
+import { statusColor} from "./status.ts";
+import {TodoActions} from "./TodoActions.tsx";
+
+type TodoProps = {
+    todo: Todo
+    getTodos: () => void
+}
+
+
+export function Todo({todo, getTodos}: TodoProps) {
+    return (
+        <div className="bg-gray-100 border border-gray-300 rounded-lg shadow p-4 flex flex-col gap-2">
+            <div className="flex justify-end">
+                 <span className={`px-2 py-1 rounded-full text-xs font-bold self-start ${statusColor[todo.status]}`}>
+                    {todo.status}
+                 </span>
+            </div>
+            <div className="text-black font-semibold text-lg">{todo.description}</div>
+
+            <TodoActions todo={todo} getTodos={getTodos}/>
+        </div>
+    )
+}
